48cd072506cc20ba591daf8263e98e9cad415908
GediminasGricius <g.gricius@ituostas.lt>
2021-08-18 19:05:15 +0300

    20210818_1

--- 20210818/kalendorius/app.js
+++ 20210818/kalendorius/app.js
@@ -1,22 +1,28 @@
 const http=require('http'); //Prisidedame HTTP moduli
 const fs=require('fs');     //Prisidedame FS moduli
+const path=require('path');
 
 //Sukuriam serveri ir paduodame f-ją kuri aptarnaus vartotojų užklausas
 const server=http.createServer((req, res)=>{
     const url=req.url;         //  /informacija.txt
     const method=req.method;
-    
-    
     let file='./public'+url;   //Turime kintamaji su failo pavadinimu: ./public/informacija.txt
+
+   
     //Patikrinsim ar failas egzistuoja ir ar jis yra failas
     if (fs.existsSync(file) && fs.lstatSync(file).isFile()){
-        let stream=fs.readFileSync(file,'utf-8'); //Nuskaitome failo turinį į kintamąjį stream
-        res.setHeader('Content-Type', 'text/css'); //Nusiuntėme headerį
+        let stream=fs.readFileSync(file); //Nuskaitome failo turinį į kintamąjį stream
+        const ext=path.parse(file).ext;   // Paimama failo galunė
+        //Jei failo glune css, server sakys kad persiunčia css faila
+        if (ext=='.css') res.setHeader('Content-Type', 'text/css'); 
+        //Jei failo glune png, server sakys kad persiunčia png paveiksla
+        if (ext=='.png') res.setHeader('Content-Type', 'image/png'); 
+        if (ext=='.jpg') res.setHeader('Content-Type', 'image/jpeg'); 
+        if (ext=='.ico') res.setHeader('Content-Type', 'image/jpeg'); 
+        
         res.write(stream); //Išvedėme vartotojui failą
         return res.end(); //Uždarome persiuntima ir nutraukiame f-ją
     }
-
-    
     let stream=fs.readFileSync('./template/index.html','utf-8'); //Nuskaitome failo turinį į kintamąjį stream
     res.setHeader('Content-Type', 'text/html'); //Nusiuntėme headerį
     res.write(stream); //Išvedėme vartotojui failą
