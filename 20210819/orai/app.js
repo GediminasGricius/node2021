2adf2150b6da9edc066c8a2102dbe3b74724ddc5
GediminasGricius <g.gricius@ituostas.lt>
2021-08-19 22:02:51 +0300

    20210819_06

--- 20210819/orai/app.js
+++ 20210819/orai/app.js
@@ -1,4 +1,20 @@
 const forecast=require('./forecast');
 
-forecast('klaipeda');
+function isveskKaiGausiDuomenis(data){
+    console.log("APP.JS GAVO DUOMENIS");
+    console.log(data); 
+}
+
+//Iškviečiame funckiją forecast su kintamaisiais:
+// 1 - miestas
+// 2 - funkcija kuri bus įvykdyta tuomet kai API atsiųs duomenis
+forecast('klaipeda', isveskKaiGausiDuomenis);
+
+/*
+forecast('klaipeda', (data)=>{
+    console.log("APP.JS GAVO DUOMENIS");
+    console.log(data); 
+});
+*/
+console.log("Sistema baige darba");
 
