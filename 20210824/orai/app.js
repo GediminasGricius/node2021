73c846c0f71fc12a43f819e880aff9085089d50e
GediminasGricius <g.gricius@ituostas.lt>
2021-08-24 20:36:20 +0300

    20210824_5

--- 20210824/orai/app.js
+++ 20210824/orai/app.js
@@ -1,27 +1,60 @@
 const forecast=require('./forecast');
+const http=require('http');
+const fs=require('fs');
+const path=require('path');
+
+//Susikuriame serverį
+const server=http.createServer((req,res)=>{
+    let url=req.url;
+    
+    if (url==='/'){
+        let stream=fs.readFileSync('./template/index.html');
+        res.setHeader('Content-type','text/html');
+        res.write(stream);
+        return res.end();
+    }
 
 
+    let getData=url.split('?');
+    if (getData[0]==='/prognoze'){
+        let place=getData[1].split('=')[1];
+
+        forecast(place,(temp)=>{
+            res.setHeader('Content-Type','text/html');
+        
+            //s - kintamajame išsaugosime sugeneruotą lentelę
+            let s='<table class="table">';
+            //su kiekvienu temp masyvo elementu vykdysime žemiau pateiktą f-ką ir prie kintamojo 
+            //s pridesime naujas eilutes 
+            temp.forEach((d)=>{
+                s+='<tr> <td>'+d.date+'</td>  <td>'+d.temperature+'</td> </tr>';
+            });
+            s+='</table>';
+        
+            //Nuskaitome failą
+            let stream=fs.readFileSync('./template/temp.html', 'utf-8');
+            stream=stream.replace('{{place}}', place);
+            stream=stream.replace('{{temperature}}', s);
+            res.write(stream);
+            res.end();
+        });
+    }
+                                      
+
+    
+    
+    
+
+});
+
+//Laukiame užklausų
+server.listen(3000, 'localhost');
+
+/*
 forecast('klaipeda', (temp)=>{
     console.log("Temperaturos prognozės: ");
     temp.forEach((d)=>{
         console.log("Diena: "+d.date+"\t Temperatura: "+d.temperature);
     });
 });
-
-
-/*
-https://api.frankfurter.app/2021-08-13..2021-08-23?from=EUR&to=USD
-let end=new Date().toISOString().slice(0,10);
-let start=new Date();
-start.setDate( start.getDate()-10);
-start=start.toISOString().slice(0,10)
-
-
-console.log(start);
-console.log(end);
-*/
-/*
-for (const [date, value] of Object.entries(curr.rates)){
-    console.log(date+" "+value.USD)
-}
 */
