642abb65cd2996ff89f1b8fd5fba0c1bff04dc95
GediminasGricius <g.gricius@ituostas.lt>
2021-08-31 11:55:38 +0300

    20210830_2

--- 20210830/vartotojai/app.js
+++ 20210830/vartotojai/app.js
@@ -1,21 +1,25 @@
 const express=require('express');
-
+const requestIp = require('request-ip');
 const app=express();
 
+app.use(express.urlencoded({extended:false}));
+
 app.use((req, res, next)=>{
-     console.log("Atejo naujas vartotojas:");
-     console.log(req.socket.remoteAddress);
-     console.log(req.url);
+     console.log("Atejo naujas vartotojas: "+requestIp.getClientIp(req)+" "+req.url);
+    
      next();
 });
 
 app.get('/users',(req,res,next)=>{
-   res.send("<h1>Vartotojų sąrašas</h1>");
+   res.send("<form action='/add_user' method='POST'><input type='text' name='vardas'><br><input type='text' name='pavarde'><button type='submit'>Issiusti</button></form>");
 });
 
-app.get('/add_user',(req,res,next)=>{
-    res.send("<h1>Pridėti vartotoją</h1>");
+app.post('/add_user',(req,res,next)=>{
+   
+    res.send("<h1>Pridėti vartotoją</h1>"+"Vartotojo vardas: "+req.body.vardas+"<br>Pavarde:"+req.body.pavarde);
 });
+
+
 
 app.use('/',(req,res,next)=>{
     res.send("<h1>Pagrindinis puslapis</h1>");
